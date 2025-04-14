f0deabb80a70a08215055694708558e6bf71458b
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-04-14 08:10:20 +0700

    update training

--- losses/dhbtrans_loss.py
+++ losses/dhbtrans_loss.py
@@ -3,8 +3,9 @@ import torch.nn as nn
 from utils.hamming import compute_inner_product
 
 class DHBLoss(nn.Module):
-    def __init__(self, args, h_matrix, similarity):
+    def __init__(self, args):
         super(DHBLoss, self).__init__()
+        self.args = args
             
     def ce_loss(self, h_matrix, similarity):
         omega = compute_inner_product(h_matrix)
@@ -26,12 +27,12 @@ class DHBLoss(nn.Module):
         loss = torch.sum(bit_means ** 2)  # ||mean||^2
         return loss
 
-    def forward(self, h_matrix, similarity, args):
-        sigma = args.sigma
-        lamda = args.lamda 
-        alpha = args.alpha 
+    def forward(self, h_matrix, similarity):
+        gamma = self.args.gamma
+        lamda = self.args.lamda 
+        alpha = self.args.alpha 
         
-        loss = self.ce_loss(h_matrix, similarity) + sigma*self.mse_loss(h_matrix, similarity, args.q) \
+        loss = self.ce_loss(h_matrix, similarity) + gamma*self.mse_loss(h_matrix, similarity, self.args.q) \
             + lamda*self.quantization_loss(h_matrix) + alpha*self.hash_balance_loss(h_matrix)
             
         return loss
