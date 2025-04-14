ba50dccc739ea0c02888d9b4636b0a63db486f7a
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-04-14 14:43:25 +0700

    update loss

--- losses/dhbtrans_loss.py
+++ losses/dhbtrans_loss.py
@@ -12,22 +12,23 @@ class DHBLoss(nn.Module):
     def ce_loss(self, h_matrix, similarity):
         omega = compute_inner_product(h_matrix)
         log_part = torch.log1p(torch.exp(omega))
-        return (log_part - similarity * omega).mean()
+        return torch.mean(log_part - similarity * omega)
 
     def mse_loss(self, h_matrix, similarity, q):
+        q = h_matrix.shape[1]
         omega = compute_inner_product(h_matrix)
-        diff = (omega + q) / 2 - similarity * q
-        loss = torch.pow(diff, 2)
+        diff = ((omega + q) / 2 - similarity * q) ** 2
+        loss = torch.mean(diff)
         
-        return loss.mean()
+        return loss
 
     def quantization_loss(self, h_matrix):
-        return torch.sum(torch.abs(torch.abs(h_matrix) - 1.0)).mean()
+        return torch.mean(torch.abs(torch.abs(h_matrix) - 1.0))
 
     def hash_balance_loss(self, h_matrix):
-        bit_means = h_matrix.mean(dim=0)
-        loss = torch.sum(bit_means ** 2)  # ||mean||^2
-        return loss.mean()
+        bit_means = torch.mean(h_matrix, dim=0)
+        loss = 4 * torch.sum(bit_means ** 2)  # ||mean||^2
+        return loss
 
     def forward(self, h_matrix, similarity):
         gamma = torch.tensor(self.args.gamma, device=device)
--- models/model.py
+++ models/model.py
@@ -6,12 +6,12 @@ class DHBTrans(nn.Module):
     def __init__(self, args):
         super(DHBTrans, self).__init__()
         self.vit = ViTModel.from_pretrained("google/vit-large-patch16-224-in21k")
-        self.relu = nn.ReLU()
+
         self.linear = nn.Linear(1024, args.bit_size)
         
     def forward(self, x):
         outputs = self.vit(x)
         x = outputs.last_hidden_state[:, 0, :]  # <-- get represented feature
-        x = self.relu(x)
+        # x = self.relu(x)
         x = self.linear(x)
         return x
